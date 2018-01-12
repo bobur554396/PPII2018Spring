99956769363d3fa5095edaf76c50fe0fed3116cb
bobur554396 <bobur.muhsimbaev@gmail.com>
2018-01-12 16:21:03 +0600

    asd

---
+++ W1/Akshabayev_G2/Examples/Example1/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Employee
+    {
+        public string name;
+        public float wage;
+
+        public Employee()
+        {
+            name = "Em1";
+            wage = 500;
+        }
+
+        public Employee(string name, float wage)
+        {
+            this.name = name;
+            this.wage = wage;
+
+            incWage();
+        }
+
+        private void incWage()
+        {
+            wage += 200;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + wage;
+        }
+
+        
+    }
+}
---
+++ W1/Akshabayev_G2/Examples/Example1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+
+            Employee e2 = new Employee("Employee10", 400);
+            //e2.incWage();
+
+            Console.WriteLine(e2);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
