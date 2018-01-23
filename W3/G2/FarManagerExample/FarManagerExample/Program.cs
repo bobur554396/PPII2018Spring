0d70eca2efa5928b9a8b911975384b6711bb619f
bobur554396 <bobur.muhsimbaev@gmail.com>
2018-01-23 10:59:04 +0600

    asd

---
+++ W3/G2/FarManagerExample/FarManagerExample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManagerExample
+{
+    class Program
+    {
+        static void showFolderContent(DirectoryInfo cur, int pos)
+        {
+            FileSystemInfo[] data = cur.GetFileSystemInfos();
+            for(int i = 0; i < data.Length; i++)
+            {
+                if (i == pos)
+                    Console.BackgroundColor = ConsoleColor.White;
+                else
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+                if (data[i].GetType() == typeof(DirectoryInfo))
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine(data[i].Name);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int pos = 0;
+
+            DirectoryInfo dir = new DirectoryInfo(@"C:\test");
+
+            while (true)
+            {
+                Console.Clear();
+                showFolderContent(dir, pos);
+
+                ConsoleKeyInfo btn = Console.ReadKey();
+                switch (btn.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        pos--;
+                        if (pos < 0)
+                            pos = dir.GetFileSystemInfos().Length - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        pos++;
+                        break;
+                    case ConsoleKey.Enter:
+                        FileSystemInfo f = dir.GetFileSystemInfos()[pos];
+                        if (f.GetType() == typeof(DirectoryInfo)) {
+                            dir = new DirectoryInfo(f.FullName);
+                        }
+                        else { }
+                        break;
+
+                }
+            }
+
+            
+        }
+    }
+}
