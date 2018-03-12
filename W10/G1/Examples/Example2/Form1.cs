26ff55bad129620a797762075c03561af6689459
Bobur A. Mukhsimbaev <bobur.muhsimbaev@gmail.com>
2018-03-12 12:06:47 +0600

    week 10

---
+++ W10/G1/Examples/Example2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point clickedPoint = e.Location;
+            button1.Location = clickedPoint;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("asd");
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.Location.ToString();
+        }
+    }
+}
