af11a573d48979992919b136d7217b8b5f817f58
bobur554396 <bobur.muhsimbaev@gmail.com>
2018-03-26 12:29:46 +0600

    week 12

---
+++ W12/G1/Examples/Example1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        Pen pen;
+        Graphics g;
+
+        int x = 0, dx = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Red, 3);
+            g = CreateGraphics();
+            timer1.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawRectangle(pen, x, 200, 100, 100);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (x + 100 > Width || x < 0)
+                dx *= -1;
+
+            x += dx;
+
+            Refresh();
+        }
+    }
+}
---
+++ W12/G1/PaintExamples/PaintExample2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintExample2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        GraphicsPath path;
+        Pen pen;
+        Point prev;
+        Bitmap btm;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Red, 3);
+
+            // g = CreateGraphics(); // Form1 paint 
+            path = new GraphicsPath();
+            btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = btm;
+
+
+            g = Graphics.FromImage(btm);
+            g.Clear(Color.White);
+        }
+
+        /*
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            prev = e.Location;
+            //g.FillEllipse(Brushes.Red, prev.X, prev.Y, 3, 3);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                Point cur = e.Location;
+
+                g.DrawLine(pen, prev, cur);
+
+                pictureBox1.Refresh();
+
+                //prev = cur; // Pencil
+            }
+        }*/
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            prev = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            /*
+            if (e.Button == MouseButtons.Left)
+            {
+                Point cur = e.Location;
+
+                g.DrawRectangle(pen, prev.X, prev.Y, cur.X - prev.X, cur.Y - prev.Y);
+                pictureBox1.Refresh();
+            }*/
+            path.Reset();
+            if (e.Button == MouseButtons.Left)
+            {
+                Point cur = e.Location;
+                //path.AddRectangle(new Rectangle(prev.X, prev.Y, cur.X - prev.X, cur.Y - prev.Y));
+                path.AddEllipse(new Rectangle(prev.X, prev.Y, cur.X - prev.X, cur.Y - prev.Y));
+                /* Pencil 
+                path.AddLine(prev, cur);
+                */
+                
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(path != null)
+                g.DrawPath(pen, path);
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawPath(pen, path);
+        }
+    }
+}
